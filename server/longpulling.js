c834d34f9a8b475ffae95ea2f1022e2bfd86b7af
Anna <anna_agabekyan_anna@mail.ru>
2021-07-23 19:05:14 +0400

    feat: implementation of a function to receive a message

--- server/longpulling.js
+++ server/longpulling.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const events = require('events')
+const PORT = 5000;
 
-const PORT = 5000
-
-const emitter = new events.EventEmitter()
+const emitter = new events.EventEmitter();
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/get-messages', (req, res) => {
     emitter.once('newMessage', (message) => {
@@ -17,9 +17,10 @@ app.get('/get-messages', (req, res) => {
 })
 
 app.post('/new-messages', ((req, res) => {
-    const message = req.body
+    const message = req.body;
     emitter.emit('newMessage', message)
     res.status(200)
 }))
+
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
